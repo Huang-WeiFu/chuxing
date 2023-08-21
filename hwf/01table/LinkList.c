281e48122d37b5b28e5a4dd5810cfdc8bb60b8eb
HuangWF <895649329@qq.com>
2023-08-21 09:48:20 +0800

    [feature] 单链表查询

--- hwf/01table/LinkList.c
+++ hwf/01table/LinkList.c
@@ -32,7 +32,31 @@ char delLinkListElePtr(struct LNode *node, int i) {}
 char editLinkListElePtr(struct LNode *node, int i, char e) {}
 
 // 查询元素于指定位置
-char getLinkListElePtr(struct LNode *node, int i, char e) {}
+char getLinkListElePtr(struct LNode *node, int i, char e) {
+    // 临时指针指向头结点
+    struct LNode *p = node;
+    // 计数器
+    int count = 1;
+
+    /**
+     * 从第一个元素开始遍历，有两种情况发生
+     * 第一种情况是j遍历到了i的位置  --> 该情况是找到了元素
+     * 第二种情况是p指针遍历到了空   --> 该情况是索引下标超出了范围
+     */
+    while (p->next && count < i) {
+        p = p->next;
+        ++count;
+    }
+
+    // 第i个元素不存在
+    if (!p->next || count > i) {
+        return 0;
+    }
+
+    // 取第i个元素并返回
+    e = p->ch;
+    return e;
+}
 
 // 生成26字母表
 void genAlphabetPtr(struct LNode *node) {}
